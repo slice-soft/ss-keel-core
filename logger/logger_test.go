95b0027505d8817815c4d2bbeb5633def86e1866
Juan Camilo Farfan <farfan1976@hotmail.com>
2026-02-25 22:25:09 -0500

    fix(logger): correct Debug log formatting in TestDebug

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -147,7 +147,7 @@ func TestDebug(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			log, buf := newTestLogger(tt.isProduction)
-			log.Debug(tt.format)
+			log.Debug("%s", tt.format)
 
 			output := buf.String()
 			hasOutput := len(output) > 0
